cb64d6da16a8be36cb1778a646a4b2f6d62aa29b
Pepijn Verlaan <p.verlaan@ksyos.org>
2019-04-02 10:45:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# PDF.js Node Module
+
+This is the [prebuilt release](https://mozilla.github.io/pdf.js/getting_started/#download) from Mozilla's PDF.js repackaged as a node module. No changes are made, only a `package.json` is added.
+
+To update and publish, run:
+
+```
+npm run update
+npm publish
+```
---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+RELEASE=$(curl -s https://api.github.com/repos/mozilla/pdf.js/releases/latest)
+
+RELEASE_VERSION=$(echo "$RELEASE" | jq -r .name | cut -c 2-)
+
+if [[ $(echo "$RELEASE" | jq -r '.assets | length') != 1 ]]; then
+    echo "Unexpected number of assets for latest release ($RELEASE_VERSION)" >&2
+    exit 1
+fi
+
+if [[ $(echo "$RELEASE" | jq -r '.assets[0].content_type') != *zip* ]]; then
+    echo "Unexpected content type for latest release ($RELEASE_VERSION)" >&2
+    exit 1
+fi
+
+RELEASE_ASSET_URL=$(echo "$RELEASE" | jq -r '.assets[0].browser_download_url')
+
+curl -L $RELEASE_ASSET_URL -o latest.zip
+rm -rf build web LICENSE
+unzip latest.zip
+rm latest.zip
+sed -i "/version/s/: .*/: \"$RELEASE_VERSION\",/" package.json
+
+echo
+echo "Now check that things are okay and run: npm publish"
